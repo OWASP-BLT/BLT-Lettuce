c0fa982474e2a04bc05876685022bb1b0a52e044
DonnieBLT <128622481+DonnieBLT@users.noreply.github.com>
2024-03-22 18:39:33 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ app = Flask(__name__)
 
 slack_events_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'], "/slack/events", app)
 client = WebClient(token=os.environ['SLACK_TOKEN'])
-#test
-
+#testtt
+ 
 @app.route('/update_server', methods=['POST'])
 def webhook():
     if request.method == 'POST':
