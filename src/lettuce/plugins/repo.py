3c4986a98677a42c9449dc02e985111130eb8283
Sarthak5598 <sarthak5598sharma@gmail.com>
2024-06-06 00:34:17 +0530

    tried to shift to slack-machie

---
+++ src/lettuce/plugins/repo.py
@@ -0,0 +1,28 @@
+import json
+
+import requests
+from machine.plugins.base import MachineBasePlugin
+from machine.plugins.decorators import command
+
+repo_json_path = "/home/DonnieBLT/BLT-Lettuce/repo.json"
+with open(repo_json_path) as f:
+    repos_data = json.load(f)
+
+
+class RepoPlugin(MachineBasePlugin):
+    @command("/project")
+    async def project(self, command):
+        data = requests.form
+        text = data.get("text")
+        user_name = data.get("user_name")
+        tech_name = text.strip().lower()
+
+        repos = repos_data.get(tech_name)
+
+        if repos:
+            repos_list = "\n".join(repos)
+            message = f"Hello {user_name}, you can implement your '{tech_name}' knowledge here:\n{repos_list}"
+        else:
+            message = f"Hello {user_name}, the technology '{tech_name}' is not recognized. Please try again."
+
+        command.say(message)
---
+++ src/local_settings.py
@@ -0,0 +1,11 @@
+from dotenv import dotenv_values
+
+secrets = dotenv_values(".secrets")
+
+SLACK_APP_TOKEN = secrets.get("SLACK_APP_TOKEN")
+SLACK_BOT_TOKEN = secrets.get("SLACK_BOT_TOKEN")
+
+PLUGINS = (
+    "lettuce.plugins.project.ProjectPlugin",
+    "lettuce.plugins.repo.RepoPlugin",
+)
